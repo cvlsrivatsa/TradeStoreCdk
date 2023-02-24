98ae525d715900ead0d35162a2c022aacdf61b05
Srivatsa Chivukula <vatsac@gmail.com>
2023-02-24 00:20:15 -0800

    Initial commit

---
+++ test/trade_store_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TradeStoreCdk from '../lib/trade_store_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/trade_store_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TradeStoreCdk.TradeStoreCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
